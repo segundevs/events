1f3f5454a989acbdb2071551f76669ecb5989118
segundevs <tushbash@gmail.com>
2021-12-03 13:02:03 -0800

    added header and pages

--- events-client/src/App.js
+++ events-client/src/App.js
@@ -1,5 +1,14 @@
 import { createClient } from "contentful";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useEffect, useState } from "react";
+
+//Components
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Details from "./pages/Details/Details";
+import Ticket from "./pages/Ticket/Ticket";
+import Checkout from "./pages/Checkout/Checkout";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   const [events, setEvents] = useState([]);
@@ -9,20 +18,37 @@ function App() {
     accessToken: process.env.REACT_APP_ACCESS_TOKEN,
   });
 
-  const getData = async () => {
-    const res = await client.getEntries({ content_type: "event" });
-    setEvents(res.items);
-  };
-
   useEffect(() => {
+    const getData = async () => {
+      const res = await client.getEntries({ content_type: "event" });
+      setEvents(res.items);
+    };
     getData();
   }, []);
 
   return (
-    <div>
-      {console.log(events)}
-      <h1>Hello from contentful</h1>
-    </div>
+    <Router>
+      <Header />
+      <div className="app__container">
+        <Switch>
+          <Route path="/" exact>
+            <Home events={events} />
+          </Route>
+          <Route path="/details/:slug" client={client}>
+            <Details />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/ticket">
+            <Ticket />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
