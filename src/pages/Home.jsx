768c9fc24d36316e9447c8ef520e04c0e84a6c69
segundevs <tushbash@gmail.com>
2021-12-21 11:17:25 -0800

    added favicon and preloading screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Card from '../components/Card';
-import { Oval } from 'react-loading-icons'
+import { Oval } from 'react-loading-icons';
 
 const Home = ({events, loading, err}) => {
   return (
     <div className="event_container">
       {loading && <div className="home_loading-spinner"><Oval stroke="#0a014a" strokeWidth={4}/></div>}
-      {err && <div>{err}</div>}
+      {err && <div className='home_error-msg'>{err}</div>}
       {events.map(event => (
         <React.Fragment key={event.sys.id}>
           <Card event={event}/>
